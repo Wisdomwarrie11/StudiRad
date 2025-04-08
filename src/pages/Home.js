c5abfe0a27c7156d988152010dd842acdd1a535d
wisdom <wisdomwarrie11@gmail.com>
2025-04-08 09:30:02 +0100

    changes in the hero content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,8 +30,8 @@ const Home = () => {
             <img src="smiling.jpeg" alt="Hero" className="img-fluid hero-image" />
           </div>
           <div className="col-lg-6 text-left text-center text-lg-start">
-            <h3 style={{color: 'rgb(24, 59, 78)'}} className="hero-title fw-bold">Transform Your <span style={{color: 'rgb(221, 168, 83)'}}>Radiography</span> Journey.</h3>
-            <p  style={{backgroundColor: 'rgb(24, 59, 78)', color: 'white', paddingLeft: '10px', borderRadius: '10px'}} className="hero-subtitle"><strong>We make your medical Journey worthwhile...</strong></p>
+            <h3 style={{color: 'rgb(24, 59, 78)'}} className="hero-title fw-bold">Welcome to Stud<span style={{color: 'rgb(221, 168, 83)'}}>i</span>Rad</h3>
+            <p  style={{backgroundColor: 'rgb(24, 59, 78)', color: 'white', paddingLeft: '10px', borderRadius: '10px'}} className="hero-subtitle"><strong>Transfrom Your Radiography Journey...</strong></p>
             <a style={{color: 'black'}} href="/registration"><button style={{backgroundColor: 'rgb(221, 168, 83)', width: '200px'}} className="btn mt-3"><strong>Join a class</strong></button></a>
           </div>
           
